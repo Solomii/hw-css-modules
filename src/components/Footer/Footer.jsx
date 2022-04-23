a720e7a72a2ff3726968bb6d04f6e8e183b41fa8
Solomii <kushniryksolomiya@gmail.com>
2022-04-23 21:11:55 +0300

    [#0] Work in progress

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,34 +1,35 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 
+import "../Styles/Footer.scss"
+
 export const Footer = () => {
   return (
-    <footer>
-      <section>
-        <img  src={require("../../img/logo_footer.jpg")} alt="Footer Logo" />
-        <p>THE BEST REAL ESTATE IN SPAIN</p>
-      </section>
-      <section>
-        <div>
+    <footer className="footer">
+      <section className="footer__section__one">
+        <div className="footer__section__one__box__logo">
+          <img src={require("../../img/logo_footer.jpg")} alt="Footer Logo" />
+          <p> <span>THE BEST</span> REAL ESTATE IN SPAIN</p>
+        </div>
+         <div className="footer__menu">
           <a href="#">All cities</a>
           <a href="#">Blog</a>
           <a href="#">Video</a>
           <a href="#">Contacts</a>
         </div>
-        <div>
-          <div>
-            <h5>Contacts:</h5>
+      </section>
+      <section className="footer__section__two">
+        <div className="footer__section__two__contact">
+           <h5>Contacts:</h5>
             <ul>
               <li><a href="#">+34(965) 020-784</a></li>
               <li><a href="#">sales@wtgspain.com</a></li>
-              <li><a href="#">Centro Comercial Playa Flamenca C/Niagara,63 Local 60/PB,first floor, 03189 - Orihuela Costa(Alicante),Spain</a></li>
+              <li><a href="#"className>Centro Comercial Playa Flamenca C/Niagara,63 Local 60/PB,first floor, 03189 - Orihuela Costa(Alicante),Spain</a></li>
             </ul>
-            <div>
               <p>Reating 4.9/5 ##### 820 votes</p>
-            </div>
-          </div>
-          <div>
-            <h5>Type</h5>
+        </div>
+        <div className="footer__section__two__type">
+            <h5>Type:</h5>
             <ul>
               <li><a href="#">Appartaments</a></li>
               <li><a href="#">Penthouses</a></li>
@@ -38,25 +39,25 @@ export const Footer = () => {
               <li> <a href="#">Duplexes</a></li>
               <li> <a href="#">Villas</a></li>
             </ul>
-          </div>
-          <div>
+        </div>
+        <div className="footer__section__two__sotial__medias">
             <h5>Follow us:</h5>
-            <div>
+            <div className="footer__box__sotial__media">
               <a href="#">Facebook</a>
               <a href="#">Linkedin</a>
               <a href="#">YouTube</a>
               <a href="#">Instagram</a>
-            </div>
-            <div>
-              <form action="">
-                <input type="text" placeholder="Your email" />
-                <button>Subscribe</button>
-              </form>
-            </div>
+          </div>
+          <div>
+            <form action="">
+              <p>Subscribe to new objects?</p>
+              <input type="text" placeholder="Your email" />
+              <button>Subscribe</button>
+            </form>
           </div>
         </div>
       </section>
-      <section>
+      <section className="footer__section__bottom">
         <p>The company "WTG Spain" specializes in the investment and sale of real estate in Spain, and the provision of legal and consulting services.
           All rights reserved."WTG Spain" - The Best Real Estate in Spain.© Copyright 2007-2020.
         </p>
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,27 +2,30 @@
 /* eslint-disable jsx-a11y/heading-has-content */
 import React from "react";
 
+import "../Styles/Main.scss"
+
 export const Main = () => {
   return (
-    <main>
-      <section>
+    <main className="main">
+      <section className="main__section__one">
         <div>
           <h3>WTG SPAIN</h3>
-          <h2>Do you need some help?</h2>
+          <h2>Do you need <span>some help</span>?</h2>
           <p>Contact us right now and our team will do eveereything to help you</p>
           <button>Contact us</button>
         </div>
-        <div>
+        <div className="main__section__one__picture">
           <img src={require("../../img/contact svg.jpg")} alt="woman" />
         </div>
       </section>
-      <section>
-        <p>We are always in touch and will be happy to resolve any of your questions</p>
+      <section className="main__section__two">
+        <h4>We are always in touch and will be happy to resolve any of your questions</h4>
         <div>
-          <div>
+          <div className="main__box__contact__map">
+          <div className="main__box__map"> 
             <img src={require("../../img/Screen Shot 2020-03-05 at 11.53 1.jpg")} alt="map" />
           </div>
-          <div>
+          <div className="main__box__contact">
             <h3>Contact us</h3>
             <form action="">
               <input type="text" placeholder="Name" />
@@ -32,25 +35,26 @@ export const Main = () => {
               <button>Send</button>
             </form>
             <p>Privacy policy</p>
+            </div>
           </div>
         </div>
       </section>
-      <section>
-        <div>
-          <div>
+      <section className="main__section__three">
+        <div className="main__contact__box__one">
+          <div className="main__contact__phone">
             <h3>Phone</h3>
             <a href="#">+34965020784</a>
           </div>
-          <div>
+          <div className="main__contact__office__address">
             <h3>Office in Spain</h3>
             <p>Centro Comercial Playa Flamenca C/Niagara,63 Local 60/PB,first floor, 03189 - Orihuela Costa(Alicante),Spain</p>
           </div>
-          <div>
+          <div className="main__contact__office__email"> 
             <h3>E-mail</h3>
             <a href="#">sales@wtgspain.com</a>
           </div>
         </div>
-        <div>
+        <div className="main__contact__box__two">
           <h5>We are online</h5>
           <div>
             <ul>
